d757559d623ab48c94c2d56da10d25fea808dc28
flexy2dd <flexy2dd@blackdev.net>
2024-05-17 18:56:13 +0200

    Update

--- morphee-lights.py
+++ morphee-lights.py
@@ -20,7 +20,6 @@ from pathlib import Path
 
 from modules import core
 from modules import constant
-from modules import microphone
 
 from adafruit_led_animation.animation.volume            import Volume
 from adafruit_led_animation.animation.sparklepulse      import SparklePulse
@@ -70,48 +69,6 @@ animationParams = []
 animationTime = 86400
 animationBrightness = 50
 exit = False
-
-# ===========================================================================
-# visualise
-# ===========================================================================
-def microphone_update(audio_samples):
-    global y_roll, prev_rms, prev_exp, prev_fps_update
-    # Normalize samples between 0 and 1
-    y = audio_samples / 2.0**15
-    # Construct a rolling window of audio samples
-    #y_roll[:-1] = y_roll[1:]
-    #y_roll[-1, :] = np.copy(y)
-    #y_data = np.concatenate(y_roll, axis=0).astype(np.float32)
-    
-    #vol = np.max(np.abs(y_data))
-    #if vol < constant.MIN_VOLUME_THRESHOLD:
-    #    print('No audio input. Volume below threshold. Volume:', vol)
-    #    reactive_pixels = np.tile(0, (3, constant.NUM_PIXELS))
-    #    updateReactive(reactive_pixels)
-    #else:
-    #    # Transform audio input into the frequency domain
-    #    N = len(y_data)
-    #    N_zeros = 2**int(np.ceil(np.log2(N))) - N
-    #    # Pad with zeros until the next power of two
-    #    y_data *= fft_window
-    #    y_padded = np.pad(y_data, (0, N_zeros), mode='constant')
-    #    YS = np.abs(np.fft.rfft(y_padded)[:N // 2])
-    #    # Construct a Mel filterbank from the FFT data
-    #    mel = np.atleast_2d(YS).T * dsp.mel_y.T
-    #    # Scale data to values more suitable for visualization
-    #    # mel = np.sum(mel, axis=0)
-    #    mel = np.sum(mel, axis=0)
-    #    mel = mel**2.0
-    #    # Gain normalization
-    #    mel_gain.update(np.max(gaussian_filter1d(mel, sigma=1.0)))
-    #    mel /= mel_gain.value
-    #    mel = mel_smoothing.update(mel)
-    #
-    #    # Map filterbank output onto LED strip
-    #    output = reactive_energy(mel)
-    #
-    #    reactive_pixels = output
-    #    updateReactive(reactive_pixels)
 
 # ===========================================================================
 # mqtt config
